ecc734eefe376b3b310ff1160b4409b53f5335fb
Noah Gabel <noahgabel1012@gmail.com>
2026-03-26 13:31:18 +0100

    fixes

--- frontend/src/components/ThemeRegistry.tsx
+++ frontend/src/components/ThemeRegistry.tsx
@@ -1,15 +1,42 @@
 'use client';
 
 import * as React from 'react';
+import createCache from '@emotion/cache';
+import { useServerInsertedHTML } from 'next/navigation';
+import { CacheProvider } from '@emotion/react';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/theme';
 
 export default function ThemeRegistry({ children }: { children: React.ReactNode }) {
+  const [cache] = React.useState(() => {
+    const cache = createCache({ key: 'mui' });
+    cache.compat = true;
+    return cache;
+  });
+
+  useServerInsertedHTML(() => {
+    const styles = Object.values(cache.inserted)
+      .filter((style) => typeof style === 'string')
+      .join('');
+
+    if (!styles) return null;
+
+    return (
+      <style
+        key={cache.key}
+        data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+        dangerouslySetInnerHTML={{ __html: styles }}
+      />
+    );
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
